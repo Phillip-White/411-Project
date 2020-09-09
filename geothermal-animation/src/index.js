9a02cd2c35be96ec4d48b30da9ed3a57818ca546
Phillip-White <55253467+Phillip-White@users.noreply.github.com>
2020-09-09 09:36:40 -0500

    new website

---
+++ geothermal-animation/src/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import map from './Geothermal_920am_May_6_2019.jpg';
+
+console.log(map);
+
+class Animation extends React.Component {
+
+    render() {
+        return (
+
+            <div className="page">
+
+                <div className="season">
+                    <a href="">Summer</a>
+                    <p></p>
+                    <a href="">Winter</a>
+                </div>
+                <div className="wrapper">
+
+                    <img src={map} id="map" alt="Geothermal System" />
+
+                </div>
+
+            </div>
+
+        );
+    }
+}
+
+
+
+ReactDOM.render(
+    <Animation />,
+    document.getElementById('root')
+);
+
