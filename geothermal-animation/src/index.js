b9e2e1226e2be265a00b162edaaee04fb83a31d2
Phillip-White <55253467+Phillip-White@users.noreply.github.com>
2020-09-15 10:18:12 -0500

    added database

--- geothermal-animation/src/index.js
+++ geothermal-animation/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import map from './Geothermal_920am_May_6_2019.jpg';
+import map from './Geothermal Schematic.jpg';
 
 console.log(map);
 
