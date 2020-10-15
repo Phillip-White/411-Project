372d4f8854daaf4004c13dcc2c891e7f625e2c91
Phillip-White <55253467+Phillip-White@users.noreply.github.com>
2020-10-14 22:21:13 -0500

    pull from origin

---
+++ geothermal-animation/src/Seasons.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Nav from './Nav'
+import PopUp from './PopUp'
+
+class Seasons extends React.Component {
+
+    popUpRender(pass) {
+        if (this.props.show) {
+            return <PopUp pic={pass[0]} text={pass[1]} />
+
+        }
+    }
+
+    render() {
+        const pass = [this.props.pic, this.props.text]
+        const popUpRender = this.popUpRender(pass)
+
+        return (
+            <div className="season">
+                <Nav />
+                {popUpRender}
+            </div>
+        )
+    }
+}
+
+export default Seasons
