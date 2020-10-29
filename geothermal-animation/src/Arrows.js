d2c51c0cf8a79be93d8344bc106b3766aae027eb
Phillip-White <55253467+Phillip-White@users.noreply.github.com>
2020-10-29 11:13:03 -0500

    add rest of arrows with animation. need to work on order some

--- geothermal-animation/src/Arrows.js
+++ geothermal-animation/src/Arrows.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { arrows } from './onload'
 import animationOrder from './data/AnimationOrder'
-import animaitonOrder from './data/AnimationOrder'
 
 class Arrows extends React.Component {
     constructor(props) {
@@ -36,7 +35,7 @@ class Arrows extends React.Component {
         this.setState({
             prev:this.state.curr
         })
-        if (this.state.step < animaitonOrder.length -1) {
+        if (this.state.step < animationOrder.length -1) {
             this.setState({
                 step: this.state.step + 1
             })
@@ -58,12 +57,20 @@ class Arrows extends React.Component {
     }
 
     render() {
-        let pic = 'blue_flow_arrow'
-        if(this.props.season==="Summer"){
+        let pic 
+        if (this.props.season === "Summer" && this.props.item._id.startsWith("o")) {
             pic = 'blue_flow_arrow'
-       }else{
+        }
+        else if (this.props.season === "Summer" && this.props.item._id.startsWith("i")) {
             pic = 'blue_flow_arrow2'
-       }
+        }
+        else if (this.props.season === "Winter" && this.props.item._id.startsWith("o")) {
+            pic = 'blue_flow_arrow2'
+        }
+        else {
+            pic = 'blue_flow_arrow'
+        }
+
         let arrow =  require('./pics/' + pic + '.png')
         const arrowStyle = {
             left: this.props.item.left,
