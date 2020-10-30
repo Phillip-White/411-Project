7bfae17f72f7e30e6eb7d5468c468a7c9d1ac990
Orethafrancis7 <54564739+Orethafrancis7@users.noreply.github.com>
2020-10-30 13:17:31 -0500

    arrow rotation

--- geothermal-animation/src/Map.js
+++ geothermal-animation/src/Map.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import Arrows from './Arrows'
-import MoreArrows from './MoreArrows'
 import ClickAreas from './ClickAreas'
 import Schematic from './pics/Geothermal Schematic.jpg'
 import ArrowList from './data/ArrowList'
-import MoreArrowList from './data/MoreArrowList'
 import ClickAreaList from './data/ClickAreaList'
 import { MapInteractionCSS } from 'react-map-interaction';
 import arrows from './onload.js'
@@ -15,7 +13,6 @@ class Map extends React.Component {
 		super()
 		this.state = {
 			listofarrows: ArrowList,
-			listofmorearrows: MoreArrowList,
 			listofclick: ClickAreaList
 		}
 	}
@@ -24,7 +21,6 @@ class Map extends React.Component {
 
 		const click = this.props.click
 		const arrowData = this.state.listofarrows.map(item => <Arrows key={item.id} item={item} />)
-		const moreArrowData = this.state.listofmorearrows.map(item => <MoreArrows key={item.id} item={item} />)
 		const clickData = this.state.listofclick.map(area => <ClickAreas key={area.id} area={area} click={click} />)
 
 		return (
