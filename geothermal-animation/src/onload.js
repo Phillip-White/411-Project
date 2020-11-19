89aaa4ba4decad007c2c154d30434ea811125bc2
Phillip-White <55253467+Phillip-White@users.noreply.github.com>
2020-11-19 17:16:42 -0600

    font edits

--- geothermal-animation/src/onload.js
+++ geothermal-animation/src/onload.js
@@ -1,3 +1,6 @@
+var pxToVw = 100 / (document.documentElement.clientWidth);
+var pxToVh = 100 / (document.documentElement.clientHeight);
+
 export function arrowTranspose(id) {
     var picWidth = window.innerWidth * .8;
     var picHeight = (picWidth/14400) * 10800;
@@ -16,13 +19,14 @@ export function arrowTranspose(id) {
 }
 
 export function transposeAreaSize() {
-    var picWidth = window.innerWidth * .8;
-    var picHeight = (picWidth / 14400) * 10800;
+    var map = document.getElementById("map");
+    var picWidth = map.clientWidth;
+    var picHeight = map.clientHeight;
 
     var area = document.getElementsByClassName("area");
 
     var i;
-    for (i = 0; i<area.length; i++) {
+    for (i = 0; i < area.length; i++) {
         var x1 = area[i].style.left.match(/\d+/g);
         var y1 = area[i].style.top.match(/\d+/g);
         var x2 = area[i].style.width.match(/\d+/g);
@@ -31,12 +35,12 @@ export function transposeAreaSize() {
         x1 = (x1 / 14400) * picWidth;
         y1 = (y1 / 10800) * picHeight;
         x2 = (x2 / 14400) * picWidth;
-        y2 = (y2 / 10800) * picHeight; 
+        y2 = (y2 / 10800) * picHeight;
 
-        area[i].style.left = x1 + 'px';
-        area[i].style.top = y1 + 'px';
-        area[i].style.width = x2 + 'px';
-        area[i].style.height = y2 + 'px';
-        
+        area[i].style.left = (x1 * pxToVw) + 'vw';
+        area[i].style.top = (y1 * pxToVh) + 'vh';
+        area[i].style.width = (x2 * pxToVw) + 'vw';
+        area[i].style.height = (y2 * pxToVh) + 'vh';
+
     }
 }
